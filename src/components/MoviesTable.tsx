6478f5fff0cfb00c4627a56d6153bd1cb3e27497
r-ILIASS <iliass.root@gmail.com>
2022-07-26 18:06:40 +0100

    feat(MoviesTable): hide Delete button if there is no user

--- src/components/MoviesTable.tsx
+++ src/components/MoviesTable.tsx
@@ -2,6 +2,9 @@ import { MovieType } from "../lib/types";
 import EmptyHeart from "../assets/empty-heart.svg";
 import FullHeart from "../assets/full-heart.svg";
 
+// TODO: get the user from the state after auth
+const user = false;
+
 interface Props {
   data: MovieType[];
   handleLike: Function;
@@ -18,7 +21,7 @@ const MoviesTable = ({ data, handleLike }: Props) => {
             <th>Stock</th>
             <th>Rate</th>
             <th></th>
-            <th></th>
+            {user && <th></th>}
           </tr>
         </thead>
         <tbody>
@@ -27,7 +30,10 @@ const MoviesTable = ({ data, handleLike }: Props) => {
               <td>{element.title}</td>
               <td>{element.genre.name}</td>
               <td>{element.numberInStock}</td>
-              <td>{element.dailyRentalRate}</td>
+              <td>
+                <span className="text-xs">$ </span>
+                {element.dailyRentalRate}
+              </td>
               <td className="min-w-[54px]">
                 {element.isLiked ? (
                   <img
@@ -43,9 +49,11 @@ const MoviesTable = ({ data, handleLike }: Props) => {
                   ></img>
                 )}
               </td>
-              <td>
-                <span className="btn-delete">Delete</span>
-              </td>
+              {user && (
+                <td>
+                  <span className="btn-delete">Delete</span>
+                </td>
+              )}
             </tr>
           ))}
         </tbody>
