a93fcf722649f60e68560118787dde9e9308a7bc
r-ILIASS <iliass.root@gmail.com>
2022-07-26 19:48:58 +0100

    fix(PaginationControls): eyecandy fix

--- src/components/common/PaginationControls.tsx
+++ src/components/common/PaginationControls.tsx
@@ -26,8 +26,8 @@ const PaginationControls = ({
           className={`${
             currentPage === page && "bg-blue-500 text-white font-semibold"
           } ${
-            currentPage !== page && "border"
-          } border-gray-400 rounded-full w-[2.3rem] h-[2.3rem] cursor-pointer hover:scale-95 duration-50 grid place-items-center text-xs text-gray-400`}
+            currentPage !== page && "border text-gray-400"
+          } border-gray-400 rounded-full w-[2.3rem] h-[2.3rem] cursor-pointer hover:scale-95 duration-50 grid place-items-center text-xs`}
         >
           {page}
         </span>
